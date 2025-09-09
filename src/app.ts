4982dc750c808364003ddb6d61048c0daddecf66
Teloxa <moralesdvd03@hotmail.com>
2025-09-08 20:06:24 -0600

    fixed: everything works

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from 'dotenv';
 import router from "./routes/empleado.routes";
+import cors from 'cors';
 
 dotenv.config()
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Servidor funcionando')
